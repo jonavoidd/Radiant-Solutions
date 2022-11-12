3fdcf2a7e77c3d8390b5d655d296ed04d40de723
Kurt_Jonathan_Gozano <kurtjonathangozano@gmail.com>
2022-11-12 19:52:40 +0800

    Updated code

--- food.py
+++ food.py
@@ -1,11 +1,14 @@
-class FoodPrice:
-    def __init__(self, price, portion):
+class Food:
+    def __init__(self, price, portion, state):
         self.price = price 
         self.portion = portion
+        self.state = state
 
     def get_price(self):
         return self.price
 
-    def get_amount(self):
+    def get_portion(self):
         return self.portion
 
+    def get_state(self):
+        return self.state
--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-from food import * 
+from food import Food
+from getpass import getpass
 
 admin = 1
 password = 'password'
 customer = 2
 
-print("(1) ---admin---\n(2) ---customer---")
-user = int(input(''))
+print("1 ---admin---\n2 ---customer---\n")
+user = int(getpass("Choose between 1 or 2:"))
 
-food = []
+while True:
+    if user == admin:
+        pwd = str(getpass("\nPlease enter password: "))
+        
+        if pwd == password:
+            print("\n\nWelcome back admin.")
+            print("\n1 ---Bills\n2 ---Profits\n3 ---Sales")
 
-#if user == admin:
-    
+            admin_action = int(getpass("Which do you want to enter? "))
+        
+            #using break for now as we wait for the data, i.e prices of foods, etc.
+            if admin_action == 1:
+                break
+            if admin_action == 2:
+                break
+            if admin_action == 3:
+                break
+        
+        else:
+            print("\n\nWrong password!")
+            break
+
+    if user == customer:
+        print("\n\nWelcome to our humble establishment our valued guest!")
+        break
