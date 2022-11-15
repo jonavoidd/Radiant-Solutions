82101b810964f99d43e2b9d11a035037eda3a05a
oh ten <118288512+ohteyn@users.noreply.github.com>
2022-11-16 02:07:12 +0800

    Update food.py

--- food.py
+++ food.py
@@ -13,10 +13,72 @@ class Food:
     def get_state(self):
         return self.state
 
-sinigang_na_dolphin_ala_kurt= Food(69,'whole','hot')
-hipon_ala_papa_justin= Food(45,'serve','hot')
-humbang_hipopotamus_ala_charles= Food(75,'slice','hot')
-lumpiang_dragon_ala_sheena= Food(20,'piece','hot')
-piniritong_ukoy_ala_arzey= Food(35,'piece','hot')
-   
     
+#main_dish
+butter_chicken= Food(300,'serve','hot')
+spicy_pork_vindaloo= Food(300,'serve','hot')
+sticky_tamarind_chicken= Food(300,'serve','hot')
+palak_paneer= Food(300,'serve','hot')
+crispy_pata= Food(300,'serve','hot')
+    
+    
+#rice
+basmati_rice= Food(50,'cup','hot')
+scallion_rice= Food(50,'cup','hot')
+white_rice= Food(50,'cup','hot')
+mexican_rice= Food(50,'cup','hot)
+pickled_rice= Food(50,'cup','hot)
+
+
+#side_dish
+green_salad= Food(120,'serve','hot')
+roasted_veggies= Food(120,'serve','hot')
+mashed_potatoes= Food(120,'serve','hot')
+green_beans= Food(120,'serve','hot')
+pickled_beets= Food(120,'serve','hot')
+     
+    
+#pastas
+lasgna= Food(150,'serve','hot')
+carbonara= Food(150,'serve','hot')
+vegetable_pasta= Food(150,'serve','hot')
+ravioli= Food(150,'serve'.'hot')
+macaroni_cheese= Food(150,'serve','hot')
+
+    
+#pastries
+cranberry_muffins= Food(100,'serve','hot')
+capuccino_muffins= Food(100,'serve','hot')
+monkey_bread= Food(100,'serve','hot')
+beignets= Food(100,'serve','hot')
+caramel_rolls= Food(100,'serve','hot')
+    
+ 
+#soups   
+gazpacho= Food(120,'serve','hot')
+harira= Food(120,'serve','hot')
+minestrom= Food(120,'serve','hot')
+laksa= Food(120,'serve','hot')
+caldo_verde= Food(120,'serve','hot')
+                   
+                   
+#desserts
+brownie_sundae= Food(120,'serve','cold')
+coffee_pie= Food(120,'serve','cold')
+leche_flan= Food(120,'serve','cold')
+grasshopper_pie= Food(120,'serve','cold')
+hazzlenut_pops= Food(120,'serve','cold')
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
