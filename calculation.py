474f3ca0a0139171c8ddfa7d11baa75c9066b0aa
Kurt_Jonathan_Gozano <kurtjonathangozano@gmail.com>
2022-11-15 12:33:55 +0800

    added another module

---
+++ calculation.py
@@ -0,0 +1,19 @@
+def payment():
+    money = float(input("Please enter your payment: "))
+
+def total():
+    cost = float(input("Your total payment is: "))
+
+def ask_toPay():
+    ready = input("Ready to pay? Type 'No' to end: ")
+    if ready == 'No':
+        return False
+    else:
+        return True
+        
+while ask_toPay():
+    money_given = payment()
+    money_needed = total()
+    money_left = money_given - money_needed
+    new_money = round(money_left, 2)
+    print("You have ₱{new_money} left. ")
